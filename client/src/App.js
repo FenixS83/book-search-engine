1b1d1b32f5a72923f84af04bbb6bb808caa7d2ee
Fenix Sampson <Fenix.sampson@gmail.com>
2021-11-29 21:56:43 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -31,3 +31,22 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
