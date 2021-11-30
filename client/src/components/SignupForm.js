cb8262dec0a0fc9753896aeb0a1e360a6fd73d6a
Fenix Sampson <Fenix.sampson@gmail.com>
2021-11-29 21:37:28 -0500

    made some changes to signupform.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -8,9 +8,11 @@ const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
-  const [validated] = useState(false);
+  const [validated, setValidated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, {error, data}] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +30,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      console.log(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);    
+    
     } catch (err) {
       console.error(err);
       setShowAlert(true);
