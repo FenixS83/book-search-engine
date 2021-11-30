6dea98931532a5afbf890d7fc6ca98cba882135c
Fenix Sampson <Fenix.sampson@gmail.com>
2021-11-29 21:49:50 -0500

    updated auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -37,14 +37,12 @@ login(idToken) {
   window.location.assign('/');
 }
 
-
-
-  logout() {
-    // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.assign('/');
-  }
+logout() {
+  // Clear user token and profile data from localStorage
+  localStorage.removeItem('id_token');
+  // this will reload the page and reset the state of the application
+  window.location.assign('/');
+}
 }
 
 export default new AuthService();
